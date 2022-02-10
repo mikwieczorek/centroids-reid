db6bf452a928861d72eb8d4535bc95a7f21ed393
Mikołaj Wieczorek <mikolaj.wieczorek@synerise.com>
2022-02-10 23:47:01 +0100

    Add parmeters and simplify

--- scripts/reid_centroid_fahion2coco/street2shop_nbs/original_format_to_coco/street2shop_to_coco.py
+++ scripts/reid_centroid_fahion2coco/street2shop_nbs/original_format_to_coco/street2shop_to_coco.py
@@ -1,30 +1,76 @@
+import argparse
+import logging
 import os
-from data_utils import *
-from data_format_transforms import *
+from pathlib import Path
+
 from coco_format_utils import COCO_json
+from data_format_transforms import transform_bbox_s2s_to_coco
+from data_utils import (
+    create_category_txt_filepaths,
+    extract_json_data,
+    load_all_images_paths_from_txt,
+    load_json,
+    merge_train_test_subsets,
+    save_json,
+)
+
+### HARDCODED
+train_filename = "train_data.txt"
+test_filename = "test_data.txt"
+train_all_filename = "train_all.txt"
+coco_json_save_name = "all_street_train.json"
 
 
-def main():
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+log = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Script to transform original Exact Street2Shop annotations to COCO format."
+    )
+    parser.add_argument(
+        "--root-dir-path",
+        help="path to root directory of Steet2Shop data",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--metadata-dir",
+        help="directory name with Steet2Shop metadata",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--images-dir",
+        help="directory name with Steet2Shop images",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--save-dir",
+        help="directory name where all new data will be saved",
+        type=str,
+        required=True,
+    )
 
     ### PARAMETERS
-    working_dir = "/data/mwieczorek/home/data/street2shop/"
-    save_dir = working_dir
-    data_dir = os.path.join(working_dir, "images")
-    images_dir = data_dir
-    save_dir = os.path.join(working_dir, "new_meta_test")
-    os.makedirs(save_dir, exist_ok=True)
-    train_filename = "train_data.txt"
-    test_filename = "test_data.txt"
-    train_all_filename = "train_all.txt"
-    train_file_path = os.path.join(save_dir, train_all_filename)
-    coco_json_save_name = "all_street_train.json"
-    coco_json_save_path = os.path.join(save_dir, coco_json_save_name)
-    meta_dir = os.path.join(working_dir, "meta")
+    args = parser.parse_args()
+
+    root_dir = Path(args.root_dir_path)
+    meta_dir = root_dir / args.metadata_dir
+    images_dir = root_dir / args.images_dir
+    save_dir = root_dir / args.save_dir
+    train_file_path = save_dir / train_all_filename
+    coco_json_save_path = save_dir / coco_json_save_name
+
+    save_dir.mkdir(exists_ok=True, parents=False)
+
+    ## TODO Categories dict maybe put in some script as constant?
     categories_dict = load_json(
         "scripts/reid_centroid_fahion2coco/street2shop_nbs/original_format_to_coco/street2shop_categories.json"
     )
 
-    # # Create single txt files with names of all images in train / test set
+    # Create single txt files with names of all images in train / test set
     extract_json_data(
         jsons_path=meta_dir,
         save_dir=save_dir,
@@ -43,18 +89,12 @@ def main():
     )
 
     # Merge train and test into one txt file
-    sub_files = [train_filename, test_filename]
     merge_train_test_subsets(
-        filenames=sub_files, save_dir=save_dir, save_filename=train_all_filename
+        filenames=[train_filename, test_filename],
+        save_dir=save_dir,
+        save_filename=train_all_filename,
     )
-
-    # Removing corrupted/missing etc files
-    # remove_duplicated_path(train_file_path=train_file_path)
     images_names = load_all_images_paths_from_txt(path=train_file_path)
-    # images_paths_list = check_if_images_exists_save(
-    #     images_paths_list=images_paths_list, train_file_path=train_file_path
-    # )
-    # remove_greyscale_images(images_paths_list)
     create_category_txt_filepaths(
         categories_dict=categories_dict,
         meta_dir=meta_dir,
@@ -73,8 +113,4 @@ def main():
     )
     coco_json.create_full_coco_json(bbox_transform_func=transform_bbox_s2s_to_coco)
     save_json(coco_json.json, coco_json_save_path, mode="w")
-    print("Street2Shop_to_coco processing finished")
-
-
-if __name__ == "__main__":
-    main()
+    log.info("Street2Shop_to_coco processing finished")
